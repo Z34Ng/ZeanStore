85c573e9fe487eecdff372ce5835281484204ecc
unknown <geminis2199@gmail.com>
2022-02-22 02:27:06 -0500

    Agregado funcionalidad para ver ordenes y detalles desde cuenta administrador

--- src/main/java/com/ecommerce/zeanstore/ClothesStore/controller/AdminController.java
+++ src/main/java/com/ecommerce/zeanstore/ClothesStore/controller/AdminController.java
@@ -4,7 +4,9 @@
  */
 package com.ecommerce.zeanstore.ClothesStore.controller;
 
+import com.ecommerce.zeanstore.ClothesStore.model.Orden;
 import com.ecommerce.zeanstore.ClothesStore.model.Producto;
+import com.ecommerce.zeanstore.ClothesStore.service.IOrdenService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.ecommerce.zeanstore.ClothesStore.service.IProductoService;
 import com.ecommerce.zeanstore.ClothesStore.service.IUsuarioService;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/administrador")   
@@ -24,6 +28,9 @@ public class AdminController {
     @Autowired 
     private IUsuarioService usuarioService;
     
+    @Autowired 
+    private IOrdenService ordenService;        
+    
     @GetMapping("")
     public String home(Model model){
         List<Producto> productos=productoService.findAll();
@@ -32,9 +39,23 @@ public class AdminController {
     }
     
     @GetMapping("/getUsersList")
-    public String getListUser(Model model){
-        
+    public String getListUser(Model model){        
         model.addAttribute("usuarios", usuarioService.findAll());
         return "administrador/usuarios";
     }
+    
+    @GetMapping("/getOrders")
+    public String getOrders(Model model){
+        model.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
+    }
+    
+    @GetMapping("/getDetailsOrder/{id}")
+    public String getDetailsOrder(@PathVariable int id, Model model){
+        Optional<Orden> orden= ordenService.findById(id);
+        
+        model.addAttribute("detallesOrden", orden.get().getDetalles());        
+        return "administrador/detalleorden";
+    }
+
 }
